8c528e024617761bb4dded143cccc90573edf1ca
Lukas Valta <valtalukas@seznam.cz>
2023-02-19 21:44:44 +0100

    version2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
   siteMetadata: {
     title: 'Domeček Jalovčí',
-    description: 'Ubytovani v penzionu Domeček Jalovčí',
+    description:
+      'Penzion Domeček Jalovčí. Ubytovani a agroturistika v malé zemědělské usedlosti v oblsati Středočeského kraje nedaleko Kamýku nad Vltavou',
     titleTemplate: '%s - Domeček Jalovčí',
     siteUrl: 'https://www.domecekjalovci.netlify.com',
     author: '@Lukas Valta <valtalukas@sezanm.cz>',
@@ -12,6 +16,14 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `std3enenx15w`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
